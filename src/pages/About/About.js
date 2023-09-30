719fb64d4808a71dc8e83a99ecef5ef39c1cbeef
Anandagadugin <gaduginanand984@gmail.com>
2023-09-30 23:25:41 +0530

    useContext in class based component

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,7 @@
 import React from "react";
 import UserClass from "./UserClass";
+import UserContext from "../../components/Context/useContext";
+
 
 class About extends React.Component {
     constructor(props) {
@@ -16,7 +18,18 @@ class About extends React.Component {
         // console.log("parent render")
         return (
             <div>
-                <UserClass name={"Anand"} location={"Gadag"} />
+                <div>
+                    LoggedIn user
+                    <UserContext.Consumer>
+                        {({ LoggedInuser }) => (
+                            <h2 className="text-md">{LoggedInuser}</h2>
+                        )}
+                    </UserContext.Consumer>
+                </div>
+                <div>
+                    <UserClass name={"Anand"} location={"Gadag"} />
+                </div>
+
             </div>
         );
     }
